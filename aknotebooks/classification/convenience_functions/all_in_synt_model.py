0c3f21a306f180b5b3676857b2f00eb6f91ceb80
AK <andreas@stratagem.co>
2018-10-01 13:35:32 +0100

    adding the all in synthetics code

--- aknotebooks/classification/convenience_functions/all_in_synt_model.py
+++ aknotebooks/classification/convenience_functions/all_in_synt_model.py
@@ -1,38 +1,28 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.grid_search import GridSearchCV
-from sklearn.model_selection import train_test_split
 import os
-from sklearn.ensemble import RandomForestClassifier,  GradientBoostingClassifier
-
-from sklearn.linear_model import RidgeClassifierCV
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.cross_validation import KFold
-#####metrics
-
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.svm import SVC
-
-
-sc = StandardScaler()
-#####metrics
-from sklearn.metrics import precision_recall_fscore_support
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-from sklearn.svm import SVC
-##### hmm imports
-from hsmm_core.prediction_engines import *
+import seaborn as sns
+from pandas.tseries.offsets import BDay
+from hsmm_core.hmm import hmm_engine
+from hsmm_core.observation_models import ExpIndMixDiracGauss
 from hsmm_core.data_utils import load_data, TradingHours
-import numpy as np
-import os
-import pickle
+from hsmm_core.data_utils import load_data, TradingHours
+from hsmm_core.feature_spaces import hmm_features
+from hsmm_core.hmm import hmm_calibration
+from hsmm_core.data_utils import load_data, TradingHours
+from hsmm_core.labelling import DataLabellingSimple
 from hsmm_core.consts import ThresholdMethod, LabellingChoice
-
+import pickle
+from hsmm_core.consts import InitialisationMethod
+import datetime as dt
+plt.style.use('ggplot')
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier,  GradientBoostingClassifier
+from sklearn.linear_model import RidgeClassifierCV
 sc = StandardScaler()
 
 ###-functions-###
@@ -191,161 +181,35 @@ class FitModels(object):
             y_pred[test_index] = clf.predict(X_test_local)
         return y_pred
 
-class PredictModels(FitModels):
-    def __init__(self):
-        pass
-
-#################
-def no_nans(label):
-    return np.sum(np.isnan(label))
 
 
-def remove_last_element(arr):
-    return arr[np.arange(arr.size - 1)]
+
+if __name__ == '__main__' :
+
+##    setting locations
+    ticker = 'test_SYNT_2states' #testing a new synthetic ticker
+
+    data_dir = os.getenv('FINANCE_DATA') #main directory
+    features_path = '/home/ak/Data/features_models/features/' #where features are saved
+    labels_path = '/home/ak/Data/features_models/labels' #where labels are saved
 
 
-class PredictModels(FitModels):
-    def __init__(self):
-        pass
 
+    ticker_labels_path = os.path.join(labels_path, ticker + '/NON_DIRECTIONAL')
 
-def no_nans(label):
-    return np.sum(np.isnan(label))
+    if not os.path.exists(os.path.join(data_dir, ticker)):
+        os.makedirs(os.path.join(data_dir, ticker))
 
+    if not os.path.exists(ticker_labels_path):
+        os.makedirs(ticker_labels_path)
 
-def remove_last_element(arr):
-    return arr[np.arange(arr.size - 1)]
-####
+        ####paths####
+    main_path = '/home/ak/Data/features_models/'
 
-ticker = 'SYNT_2states'
-features_path='/home/ak/Data/features_models/features/'
-
-####paths####
-labels_path = '/home/ak/Data/features_models/labels/NON_DIRECTIONAL/'
-main_path = '/home/ak/Data/features_models/'
-
-models_path = os.path.join(main_path,'models')
-hmm_models_path = os.path.join(models_path,'hmm_models')
-features_ticker_path = os.path.join(features_path, ticker)
-predictions_path = os.path.join(main_path, 'predictions')
-
-
-features_path = os.path.join(main_path, 'features')
-ticker_labels_path = os.path.join(labels_path, ticker)
-ticker_models_path = os.path.join(models_path, ticker)
-ticker_predictions_path = os.path.join(predictions_path, ticker)
-
-ticker_features_path = os.path.join(features_path, ticker)
-
-###
-
-# list of files
-labels_list = os.listdir(ticker_labels_path)
-
-features_list = os.listdir(ticker_features_path)
-
-###
-
-####
-data_cls = DataLoader(path_=main_path, ticker=ticker)
-idx = 1  # take first label-index for the data-frame of labels
-######
-
-
-# list of files
-labels_list = os.listdir(ticker_labels_path)
-
-features_list = os.listdir(ticker_features_path)
-
-###-HMM-#####
-n_hidden_states = 2
-
-startprob = np.array([0.6, 0.4])
-
-transmat = np.array([[0.2, 0.8], [0.6, 0.4]])
-
-
-init_params = {
-    "obs_model_params": {
-                                'obs_model_name': 'ExpIndMixDiracGauss',
-                                'em_init_method': InitialisationMethod.cluster
-
-    },
-    "hidden_model_params": {
-                                'no_hidden_states': n_hidden_states,
-                                'pi':startprob,
-                                'tpm': transmat,
-                                'em_init_method': InitialisationMethod.uniform
-    },
-    "update_tag": 'tpsml'
-}
-
-
-# start_dt = '20171002'
-
-# end_dt = '20171003'
-
-trading_hours_filter = TradingHours.only_mkt_hours
-
-data_dic = load_data(ticker, no_of_days=2) #, start_date=start_dt, end_date=end_dt)
-hmm_calibration_engine = hmm_calibration(no_parallel_procs=None,
-                                         init_params=init_params)
-
-
-trained_hmms = hmm_calibration_engine.hmm_fit_func(ticker, data_dic, trading_hours_filter,
-                                                   force_recalc=False)
-
-
-###saving trained model hmms###
-seq_model = "_".join((str(n_hidden_states),'state',"trained","hmm","models", ".pickle"))
-print("saving the model:", seq_model)
-pickle.dump(init_params, open(os.path.join(models_path,seq_model), 'wb'))
-
-models_dates = trained_hmms.keys() #dates of the trained hmm models
-#create labels?
-
-## clf fitting##
-for date, date_hmm in trained_hmms.iteritems():
-    newpath=os.path.join(features_ticker_path, str(date))
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)
-
-    feature_engine = hmm_features(date_hmm)
-    features_load = feature_engine.generate_features(data_dic[date])
-    labels_load = data_cls.ticker_labels_csv(date=date)
-    features, labels_clean = remove_nans(features_load, labels_load)
-    x_std = sc.fit_transform(features.values.astype(np.float)) #fit & transform the features
-    X_train, X_test, y_train, y_test = train_test_split( \
-        x_std, labels_clean, test_size=0.05, random_state=1, stratify=labels_clean) #probably can get rid of this
-    models_cls = FitModels(X_train, y_train)
-    best_clfs = {'SVC': models_cls.svm_clf(kernel_choice="rbf"),
-                 'RIDGE_clf': models_cls.ridge_clf(),
-                 'GBOOST': models_cls.gradient_boost_clf(),
-                 'GP_clf': models_cls.gp_clf(),
-                 'RF_clf': models_cls.random_forest_clf(),
-                 }
-    # This is sequence for the name of the best classifiers.
-    seq_clf = "_".join((str(date),labels_clean.columns.values[0],"clfs", ".pickle"))
-    print("saving the classifiers:",seq_clf)
-    pickle.dump(best_clfs, open(os.path.join(ticker_models_path,seq_clf), 'wb'))
-
-    hmm_fwd_dates = fwd_dates(_dates_list=models_dates, _key_date=date)  # create fwd out of sample dates
-
-    for fwd_date in hmm_fwd_dates:  # OOS testing
-
-        fwd_features = feature_engine.generate_features(data_dic[fwd_date])
-        fwd_labels = data_cls.ticker_labels_csv(date=fwd_date)
-        features_fwd, labels_fwd = remove_nans(fwd_features, fwd_labels)
-        x_std_fwd = sc.fit_transform(features_fwd.values.astype(np.float))  # fit & transform the features
-        y_true = labels_fwd
-        CLFs = ['SVC','RIDGE_clf','GBOOST','GP_clf','RF_clf']
-        for clf in CLFs:
-            y_predict_clf = best_clfs[clf].predict(x_std_fwd)
-            classif_rate = np.mean(y_predict_clf.ravel()== y_true.values.ravel())*100
-            print ("Classification Rate for %s: %f" % (clf,classif_rate))
-            clf_report = prec_recall_report(y_true, y_predict_clf)
-            report_name = "_".join(('performance', 'report', 'ticker', str(fwd_date), '.csv'))
-            report_loc = os.path.join(ticker_predictions_path, report_name)
-            clf_report.to_csv(report_name)
-
-
+    models_path = os.path.join(main_path, 'models')
+    ticker_models_path = os.path.join(models_path, ticker)
+    # hmm_models_path = os.path.join(models_path,'hmm_models')
+    # features_ticker_path = os.path.join(features_path, ticker)
+    # predictions_path = os.path.join(main_path, 'predictions')
+    if not os.path.exists(ticker_models_path):
+        os.makedirs(ticker_models_path)
