97fb28b17d37ec7b62cf44dc4ee3210db97f8728
AK <akoukorinis@gmail.com>
2021-08-30 21:19:11 +0100

    playing around with MKL

--- MultiKernelLearning/mkl_model_fit.py
+++ MultiKernelLearning/mkl_model_fit.py
@@ -1,7 +1,13 @@
+import mkl_data_processing as mkldp
+import os
+import pickle
+import pandas as pd
+import sys
 import sys
 from memory_profiler import profile
 import pandas as pd
-
+import numpy as np
+from collections import defaultdict
 sys.path.append('/home/ak/Documents/Research/PaperCode/singlekernelclf/')
 import jsonpickle
 from MKLpy.preprocessing import normalization, rescale_01
@@ -11,137 +17,202 @@ from MKLpy.algorithms import AverageMKL, EasyMKL
 from sklearn.svm import SVC
 from collections import OrderedDict
 import os
+from sklearn.metrics import accuracy_score
 import torch
-import time
-import pickle
-from collections import defaultdict
-import new_alternate_single_svm as nalsvm
-from mkl_data_processing import storage_location, cross_validation_results_location, evaluate_predictions, \
-    oos_results_location, return_cross_val_symbol_path, model_dates_list
+from MKLpy.model_selection import cross_val_score
 from MKLpy.multiclass import OneVsRestMKLClassifier, OneVsOneMKLClassifier
-from mkl_model_cross_validation import load_pickled_in_filename
-# evaluate the solution
-from sklearn.metrics import accuracy_score, roc_auc_score
-# MKL algorithms
+
 from MKLpy.algorithms import AverageMKL, EasyMKL, \
     KOMD  # KOMD is not a MKL algorithm but a simple kernel machine like the SVM
 
+from fileutils import new_feature_utils as nfu
+from fileutils.new_feature_utils import CreateMarketFeatures
+import multiprocessing
+from fileutils import DataLoader as DataLoader
+from fileutils import paths
+from fileutils import new_feature_utils as nfu
+from fileutils.new_feature_utils import CreateMarketFeatures
+
+
+# ------ functions ------- #
+def open_pickle_filepath(pickle_file):
+    pickle_to_file = pickle.load(open(pickle_file, "rb"), encoding='latin1')
+
+    return pickle_to_file
+
+
+def forward_Dates(list_of_keys, current_date):
+    """
+    return all the forward looking dates for each idxKey we use for training
+
+    :param list_of_keys: dates i have model dates for out of sample
+    :param current_date: current model date
+    :return: forward dates for applying the fitted model
+    """
+    lookAheadKeys = sorted(i for i in list_of_keys if i > current_date)
+    return lookAheadKeys
+
+
+# ------- paths --------- #
+
+mainPath = mkldp.paths('main')
+
+symbols = sorted(os.listdir(mkldp.paths('symbols_features')))
+jointFeatureLocation = os.path.join(mainPath, "ExperimentCommonLocs/JointLocationsDicts")
+commonLocs = os.path.join(mainPath, "ExperimentCommonLocs/")
+
+mklOOSPredictionPath = os.path.join(mainPath, "ExperimentCommonLocs/MKLOOSPredictions")
+fittedModelsPath = os.path.join(mainPath, "ExperimentCommonLocs/FittedModels")
+oosPredictionsPath = os.path.join(mainPath, "ExperimentCommonLocs/OOSPredictions")
+
+# ---------- files
+
+allFiles = os.listdir(jointFeatureLocation) # all files in the joint location area
+
+labelsAlternateLocs = {f:os.path.join(commonLocs, f) for f in os.listdir(os.path.join(mainPath, "ExperimentCommonLocs/")) if str("LabelsAlternate") in f }
+
+
+alternate_labels_nos = {1:"LabelsAlternateOne", 2:"LabelsAlternateTwo", 3:"LabelsAlternateThree",
+                        4:"LabelsAlternateFour", 5:"LabelsAlternateFive", 6:"LabelsAlternateSix"}
+# we have 6
+# alternative data types
+
+
 if __name__ == '__main__':
-    pass
-    # # pick the symbols you want to try out of sample
-    # # pick the model dates- this is important as you need to calculate the out of sample "forward" dates
-    # # pick label
-    # alternate_label = 'LabelsAlternateFive'
-    # # index of label - this can be redundant
-    # alternate_label_idx = list(nalsvm.labels_pickle_files).index(alternate_label)
-    # print(alternate_label)
-    # # clean data location
-    #
-    # for symbol in ['RDSa.L']:
-    #     print('Doing predictions!!')
-    #     clean_data_location = storage_location(symbol)
-    #     print(clean_data_location)
-    #     out_of_sample_location = oos_results_location(symbol)
-    #     print(symbol)
-    #     # model dates list
-    #     model_dates = model_dates_list(return_cross_val_symbol_path(symbol))
-    #     # location of data -->dataDrive, Clean Data Storage, and label.
-    #     pkl_file = load_pickled_in_filename(
-    #         os.path.join(clean_data_location, os.listdir(clean_data_location)[alternate_label_idx]))
-    #     date_keys = list(pkl_file.keys())
-    #     # list of out of sample dates
-    #
-    #     for model_date in model_dates:
-    #         forward_dates = nalsvm.forwardDates(date_keys, model_date)
-    #
-    #         print('---------------> Doing Model Date:', model_date)
-    #         try:
-    #             # put the features in a tensor format
-    #             Xtr = normalization(rescale_01(torch.Tensor(pkl_file[model_date][0].values)))  # fitting model
-    #             # put the labels in a tensor format
-    #             Ytr = torch.Tensor(pkl_file[model_date][1].values)
-    #             print('first bit done')
-    #             # force garbage collect
-    #             nalsvm.gc.collect()
-    #             # kernels
-    #             KLrbf = generators.RBF_generator(Xtr, gamma=[.01, .1])
-    #             # dont need the next bit
-    #             print('done with kernel')
-    #             print(forward_dates)
-    #             # base learner- use c =1 or 10
-    #             # the c and lambda values need to be picked up by the cross-val results !
-    #             base_learner = SVC(C=1)
-    #
-    #             clf = EasyMKL(lam=0.2, multiclass_strategy='ova', learner=base_learner).fit(KLrbf, Ytr)
-    #             # try ovo as
-    #             # well
-    #             mkl_avg = AverageMKL().fit(KLrbf, Ytr)
-    #             print('done')
-    #             print('the combination weights are:')
-    #             # this bit may be redundant here and we can put it somewhere else
-    #             weights_mkl = dict()
-    #             for sol in clf.solution:
-    #                 print('(%d vs all): ' % sol,
-    #                       clf.solution[sol].weights)  # dont need this loop- can make it redundant in another file
-    #                 weights_mkl[model_date] = clf.solution[sol].weights
-    #
-    #             for date in forward_dates:
-    #                 oos_results_mkl = defaultdict(dict)
-    #                 print(date)
-    #                 start = time.time()
-    #                 nalsvm.logmemoryusage("Before garbage collect")
-    #                 Xte = normalization(rescale_01(torch.Tensor(pkl_file[date][0].values)))
-    #                 Yte = torch.Tensor(pkl_file[date][1].values)
-    #                 try:
-    #                     KLte = generators.RBF_generator(Xte, gamma=[.01, .1])
-    #                     print('sorted out test dates bit done')
-    #                     nalsvm.gc.collect()
-    #                     y_pred = clf.predict(KLte)  # predictions
-    #                     y_score = clf.decision_function(KLte)  # rank
-    #                     # oos_svc_predictions = defaultdict(dict)
-    #                     accuracy = accuracy_score(Yte, y_pred)
-    #                     accuracy_file_name = "_".join((symbol, model_date, date, alternate_label, 'OOSResult.pkl'))
-    #                     print('Accuracy score: %.3f' % accuracy)
-    #                     evaluate_predictions(Yte, y_pred)
-    #                     oos_results_mkl['MKL'][date] = evaluate_predictions(Yte, y_pred)
-    #
-    #                     # average kernel as a base line
-    #
-    #                     y_preds_average = mkl_avg.predict(KLte)  # predict the output class
-    #                     y_scores_average = mkl_avg.decision_function(
-    #                         KLte)  # returns the projection on the distance vector
-    #                     average_accuracy = accuracy_score(Yte, y_preds_average)
-    #                     print('Accuracy score: %.3f' % average_accuracy)
-    #                     oos_results_mkl['AVG'][date] = evaluate_predictions(Yte, y_preds_average)
-    #                 except (ValueError, TypeError, EOFError, IndexError):
-    #                     continue
-    #
-    #         except (ValueError, TypeError, EOFError, IndexError):
-    #             # at some point for clarity we need to clean these error up.
-    #             continue
-    #
-    #         # storing the results pickle
-    #
-    #         pickle_out_filename = os.path.join(out_of_sample_location,
-    #                                            "_".join((symbol, date, str(alternate_label), '_OOS_Results.pkl')))
-    #         mkl_pickle_out = open(pickle_out_filename, 'wb')
-    #         pickle.dump(oos_results_mkl, mkl_pickle_out)
-    #         mkl_pickle_out.close()
-    #
-    #         # oos_results_mkl_df = pd.DataFrame.from_dict(oos_results_mkl)
-    #         # oos_results_mkl_df.to_pickle(pickle_out_filename)
-    #
-    #         # storing the weights
-    #
-    #         weights_out_filename = os.path.join(out_of_sample_location,
-    #                                             "_".join((symbol, date, str(alternate_label), '_OOS_Weights.pkl')))
-    #
-    #         pickle_out = open(weights_out_filename, 'wb')
-    #         pickle.dump(weights_mkl, pickle_out)
-    #         pickle_out.close()
-    #
-    #         print('Now saved: ', pickle_out_filename)
-    #         nalsvm.gc.collect()
-    #         print('done too')
-    #         end = time.time()
-    #         print(f'it took {end - start} seconds!')
+    # pick a symbol
+    symbolIdx = 10
+
+    symbol = symbols[symbolIdx]
+    print(symbol)
+
+    # isolate all the symbol Files
+    symbolFiles = [f for f in allFiles if str(symbol) in f]
+
+    for fileIdx, _ in enumerate(symbolFiles):
+        # pick a specific file out of all the symbol files
+
+        file = symbolFiles[fileIdx] # this is the specific file
+
+        select_file_path = os.path.join(jointFeatureLocation, file)  # formulate the path for the specific file
+        select_hmm_date = select_file_path.split("_")[3]  # pull out the hmm_date - strip it out
+        select_feature_label_date = select_file_path.split("_")[6]  # pull out the label_feature_date from the
+        # specific file
+
+        select_label_idx = select_file_path.split("_")[9]  # pull out the label _idx
+
+        print('You are working on symbol:----->', file.split("_")[0],
+              'with hmm date: ', select_hmm_date , ' and label feature date' , select_feature_label_date,' and label idx:', select_label_idx)
+
+        unpickled_select_file = open_pickle_filepath(select_file_path)
+        # unplickle the select file - and basically this has locations in it for features and labels
+
+        hmm_keys = sorted(list(unpickled_select_file.keys()))
+        # hmm keys - essentially for each of these dates an HMM model was fitted and then it was used out of sample for
+        # all the other dates
+        check_dict = defaultdict(dict)
+        all_good_dict = defaultdict(dict)
+
+        for hmm_date_key in hmm_keys:  # pick and hmm date
+            feature_label_keys = sorted(
+                unpickled_select_file[hmm_date_key].keys())  # each key here unlocks a feature and label set
+
+            for feature_label_date in feature_label_keys:  # make a list of all the feature dates
+                features_file_path = unpickled_select_file[hmm_date_key][feature_label_date][
+                    0]  # this is the feature path
+                labels_file_path = unpickled_select_file[hmm_date_key][feature_label_date][1]  # this is the labels path
+
+                if (os.path.isfile(features_file_path) == False) or (os.path.isfile(labels_file_path) == False):
+                    print('problem here')
+                else:
+                    print('ok----->', feature_label_date)  # if you got to this point we have data so we can mov eon
+                    labels = pd.read_csv(labels_file_path)  # open labels file
+                    label_name = str(labels.columns[labels.columns.str.contains(pat='label')].values[0])
+                    features = open_pickle_filepath(features_file_path)  # opens features file
+                    hmm_features = nfu.hmm_features_df(features)  # get the hmm features out, so unpack the tuples!
+                    print('loaded features and labels ')
+                    if hmm_features.isnull().values.all():  # checking that the HMM features are actually not null
+                        continue
+                    else: - # if features not null then start moving on!
+                        market_features_df = CreateMarketFeatures(
+                            CreateMarketFeatures(
+                                CreateMarketFeatures(df=CreateMarketFeatures(df=labels).ma_spread_duration())
+                                    .ma_spread()).chaikin_mf()).obv_calc()  # market features dataframe
+
+                        df_concat = pd.DataFrame(
+                            pd.concat([hmm_features, market_features_df], axis=1, sort='False').dropna())
+
+                        df = df_concat[df_concat[label_name].notna()]
+                        df_final = df.drop(
+                            columns=['TradedPrice', 'Duration', 'TradedTime', 'ReturnTradedPrice', 'Volume',
+                                     label_name])
+                        y_train = df[df.columns[df.columns.str.contains(pat='label')]].iloc[:, 0]  # training labels
+                        if df_final.shape[0] < 10:  # make sure it all looks reasonable
+                            print(' the ratio of classes is too low. try another label permutation')
+                            continue
+                        else:
+
+                            print("starting model fit")
+                            # put the features in a tensor format
+                            X = np.asarray(df_final.values)  # need this for torch
+                            Xtr = normalization(rescale_01(torch.Tensor(X)))  # features in a tensor format
+                            Xtr = normalization(rescale_01(torch.Tensor(pkl_file[date][0].values)))
+                            #
+                            # Ytr = torch.Tensor(y_train.values)  # put the labels in a tensor format
+                            # print('-----------------first bit done------------------')
+                            # KLrbf = generators.RBF_generator(Xtr, gamma=[.01, .1, .25,
+                            #                                              .5, .9, 1.25])  # get a few RBF Kernels ready - maybe need more here
+                            # print('done with kernel')
+                            # best_results = {}
+                            #
+                            # C_range = [0.1, 1]
+                            # lam_range = [0.2]
+                            # try:
+                            #
+                            #     for C_choice in C_range:
+                            #         base_learner = SVC(C=C_choice)  # "hard"-margin svm
+                            #         clf = EasyMKL(lam=0.2, multiclass_strategy='ova', learner=base_learner).fit(KLrbf,
+                            #                                                                                     Ytr)
+                            #         print('done')
+                            #         print('the combination weights are:')
+                            #
+                            #         for sol in clf.solution:
+                            #             print('(%d vs all): ' % sol,
+                            #                   clf.solution[
+                            #                       sol].weights)  # need to store these results somewhere
+                            #
+                            #         for lam in lam_range:  # possible lambda values for the EasyMKL algorithm
+                            #             # MKLpy.model_selection.cross_val_score performs the cross validation automatically, it may returns
+                            #             # accuracy, auc, or F1 scores
+                            #             scores = cross_val_score(KLrbf, Ytr, EasyMKL(learner=base_learner, lam=lam),
+                            #                                      n_folds=5,
+                            #                                      scoring='accuracy')  # get the cross-validation scores
+                            #             acc = np.mean(scores)
+                            #             if not best_results or best_results['score'] < acc:
+                            #                 best_results = {'C': C_choice, 'lam': lam, 'score': acc,
+                            #                                 'scores': scores}  # these should get dumped somewhere
+                            #     print('done')
+                            #     best_learner = SVC(C=best_results['C'])
+                            #     clf = EasyMKL(learner=best_learner, lam=best_results['lam']).fit(KLrbf, Ytr)
+                            #     y_pred = clf.predict(KLrbf)
+                            #     accuracy = accuracy_score(Ytr, y_pred)
+                            #     print('accuracy on the test set: %.3f, with lambda=%.2f' % (
+                            #         accuracy, best_results['lam']))
+                            #     print(scores)
+                            #
+                            #     # pickle_out_filename = os.path.join(mainPath,
+                            #     #                                    "ExperimentCommonLocs/CrossValidationResults",
+                            #     #                                    "_".join(
+                            #     #                                        (symbol, 'feature_label_date',
+                            #     #                                         str(select_feature_label_date),
+                            #     #                                         str(select_label_idx), 'hmm_date:',
+                            #     #                                         hmm_date_key, 'RBF',
+                            #     #                                         'MultiKernelSVC.pkl')))
+                            #     # pickle_out = open(pickle_out_filename, 'wb')
+                            #     # pickle.dump(best_results, pickle_out)
+                            #     # pickle_out.close()
+                            #
+                            #
+                            # except ValueError:
+                            #     continue
+                            #
+                            #
