f48f627c2635301ce025567421909bd961fcfecc
AK <akoukorinis@gmail.com>
2022-12-30 14:55:37 +0000

    adding the refactored MFDFA output

--- stylised_facts/lob_for_futures/produce_mfdfa_data_and_store.py
+++ stylised_facts/lob_for_futures/produce_mfdfa_data_and_store.py
@@ -20,10 +20,9 @@ standard_scaler = StandardScaler()
 
 sys.path.insert(0, '/directory/tothe/handshakefile/')
 
-
-def read_pkl_idx(files_loc, file_idx):
-    files = os.listdir(files_loc)
-    file_idx_loc = os.path.join(files_loc, files[file_idx])
+def read_pkl_idx(file_loc, file_idx):
+    files = os.listdir(file_loc)
+    file_idx_loc = os.path.join(file_loc, files[file_idx])
     dict_idx = pd.read_pickle(file_idx_loc)
     return dict_idx
 
@@ -45,24 +44,23 @@ def mfdfa_output(lob_df_input, bar_type_):
     raw_data = lob_df_input.pct_change_micro_price  # this is the chosen data to use
     # follows Fathon Example from here onwards
     data_input = mdf.to_agg(raw_data)
-    with np.errstate(divide='ignore'):
-        winSizes = fu.linRangeByStep(10, 2000)  # this needs to be more dynamic
-        qs = np.arange(-3, 4, 0.1)
-        revSeg = True
-        polOrd = 1
-        testClass = mdf.mfdfaquantities(data_input, winSizes, qs, revSeg, polOrd)
-        n, F = testClass.n_F_output()
-        list_H, list_H_intercept = testClass.H_and_H_intcpt_output()
-        tau = testClass.compute_mass_exponents()
-        alpha, mfSpect = testClass.compute_multi_fractal_spectrum()
+    winSizes = fu.linRangeByStep(10, 2000)  # this needs to be more dynamic
+    qs = np.arange(-3, 4, 0.1)
+    revSeg = True
+    polOrd = 1
+    testClass = mdf.mfdfaquantities(data_input, winSizes, qs, revSeg, polOrd)
+    n, F = testClass.n_F_output()
+    list_H, list_H_intercept = testClass.H_and_H_intcpt_output()
+    tau = testClass.compute_mass_exponents()
+    alpha, mfSpect = testClass.compute_multi_fractal_spectrum()
 
-        mfdfa_output_dict_ = defaultdict(dict)
-        mfdfa_output_dict_[bar_type_]['n_F'] = dict(zip(n, F))
-        mfdfa_output_dict_[bar_type_]['list_H'] = list_H
-        mfdfa_output_dict_[bar_type_]['list_H_intercept'] = list_H_intercept
-        mfdfa_output_dict_[bar_type_]['tau'] = tau
-        mfdfa_output_dict_[bar_type_]['alpha'] = alpha
-        mfdfa_output_dict_[bar_type_]['mfSpect'] = mfSpect
+    mfdfa_output_dict_ = defaultdict(dict)
+    mfdfa_output_dict_[bar_type_]['n_F'] = dict(zip(n, F))
+    mfdfa_output_dict_[bar_type_]['list_H'] = list_H
+    mfdfa_output_dict_[bar_type_]['list_H_intercept'] = list_H_intercept
+    mfdfa_output_dict_[bar_type_]['tau'] = tau
+    mfdfa_output_dict_[bar_type_]['alpha'] = alpha
+    mfdfa_output_dict_[bar_type_]['mfSpect'] = mfSpect
 
     return mfdfa_output_dict_
 
@@ -70,51 +68,40 @@ def mfdfa_output(lob_df_input, bar_type_):
 experimentsLocation = '/media/ak/T71/August11th2022Experiments/'
 expOneLocation = os.path.join(experimentsLocation, 'ExperimentOne')
 
-
 if __name__ == '__main__':
-    symbol = 'FB1'
-    symbol_input_files_loc = os.path.join(experimentsLocation, 'ExperimentInputFiles')
-    bar_type = 'tick'
-
-    symbol_files = [f for f in os.listdir(symbol_input_files_loc) if str(bar_type) in f]
+    symbol_ = 'XM1'
+    # symbol_input_files_loc_ = os.path.join(experimentsLocation, 'ExperimentInputFiles')
 
 
-    def all_in_calculations(symbol_file_idx_, bar_type_, symbol_):
-        symbol_input_files_loc = os.path.join(experimentsLocation, 'ExperimentInputFiles', symbol_)
-        symbol_files_ = [f for f in os.listdir(symbol_input_files_loc) if str(bar_type_) in f]
+    def all_in_calculations(symbol_input_files_loc, symbol_file_idx_, bar_type_):
         print('doing index', symbol_file_idx_)
-
-        bars_dict_idx = read_pkl_idx(symbol_input_files_loc, symbol_file_idx_)  # output dictionary with 4
-
-        # types
-        mfdfa_dict_output = mfdfa_output(bars_dict_idx, bar_type_)
-        test_output_loc = os.path.join(expOneLocation, str(symbol),
-
+        df =(read_pkl_idx(symbol_input_files_loc, symbol_file_idx_))
+        mfdfa_dict_output = mfdfa_output(df , str(bar_type))
+        test_output_loc = os.path.join(expOneLocation,
                                        str(symbol_) + str(symbol_file_idx_) + str(bar_type_) + "_mfdfa.pkl")
         pickle.dump(mfdfa_dict_output, open(test_output_loc, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
         print('saved')
         return mfdfa_dict_output
 
+    #
     st = time.time()
     symbol = 'XM1'
+    bar_type = 'tick'
     symbol_input_files_loc = os.path.join(experimentsLocation, 'ExperimentInputFiles', symbol)
-    bar_type = 'dollar'
-
     symbol_files = [f for f in os.listdir(symbol_input_files_loc) if str(bar_type) in f]
-
-    freeze_support()
-    # takes bar and file and produces the output
     pool = Pool(6)
     start_time = time.perf_counter()
-    # on processes change this to take the input of a symbol above!
-    processes = [pool.apply_async(all_in_calculations, args=(files_idx_, str(bar_type), symbol)) for files_idx_ in
-                 range(0, len(symbol_files))]
-    # can change this range if need be to complete the data easier?
-    # maybe make this a bit more modular?
-    result = [p.get() for p in processes]
-    finish_time = time.perf_counter()
-    print(f"Program finished in {finish_time - start_time} seconds")
-    print(result)
+    print()
+    a_args = [f for f in range(0, len(symbol_files), 1)]  # length of files
+    print(a_args)
+    b_args = ['tick']  # range of bars
+    # print(all_in_calculations(symbol_input_files_loc, 10, bar_type))
+    freeze_support()
+    # takes bar and file and produces the output
+    with Pool() as pool:
+        L = pool.starmap(all_in_calculations, [(symbol_input_files_loc, symbol_file_idx_, bar_type_)
+                                               for symbol_input_files_loc in [symbol_input_files_loc]
+                                               for symbol_file_idx_ in a_args for bar_type_ in b_args])
     et = time.time()
     elapsed = et - st
     print('Elapsed time: ', elapsed)
